736abeeb1d6b16187822ac78428844a4fef7403a
Muhammad Haris <77878998+userharis@users.noreply.github.com>
2021-09-24 11:03:14 +0500

    refactor reset button code into the displayController

--- app.js
+++ app.js
@@ -132,6 +132,12 @@ const displayController = (() => {
 		fieldBtns.forEach((btn, index) => {
 			btn.addEventListener('click', () => gameController.makeMove(index))
 		})
+		
+		// reset button
+		let reset = document.getElementById("reset");
+	        reset.addEventListener('click', function() {
+  	        window.location.reload();
+  });
 	})()
 
 	return {
@@ -301,8 +307,3 @@ const gameController = (() => {
 		setDifficulty
 	}
 })()
-
-  let reset = document.getElementById("reset");
-	reset.addEventListener('click', function() {
-  	window.location.reload();
-});
